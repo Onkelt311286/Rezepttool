b226bf8ca346084bba874358cd81921987980a00
onkelt311286 <onkelt311286@googlemail.com>
2015-02-08 10:58:43 +0100

    autocomplete eingebaut

---
+++ src/somepackage/AutoCompleteView.java
@@ -0,0 +1,28 @@
+package somepackage;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class AutoCompleteView {
+     
+    private String txt6;
+     
+    public List<String> completeText(String query) {
+        List<String> results = new ArrayList<String>();
+        for(int i = 0; i < 10; i++) {
+            results.add(query + i);
+        }
+         
+        return results;
+    }
+ 
+    public String getTxt6() {
+        return txt6;
+    }
+ 
+    public void setTxt6(String txt6) {
+        this.txt6 = txt6;
+    }
+}
