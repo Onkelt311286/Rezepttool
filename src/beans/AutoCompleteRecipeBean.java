2c5c0da70268fc0503037ff1323d8f1c9e9faeee
onkelt311286 <onkelt311286@googlemail.com>
2015-03-08 17:18:31 +0100

    handleselect erstellt nun eine neue liste anhand des filterString

--- src/beans/AutoCompleteRecipeBean.java
+++ src/beans/AutoCompleteRecipeBean.java
@@ -28,34 +28,34 @@ import util.ServeletBackbone;
 @ManagedBean
 public class AutoCompleteRecipeBean {
 
-  private String filterString;
+  private String       filterString;
   private List<Recipe> recipes;
-  
-//  @PostConstruct
-//  public void init() {
-//	recipes = ServeletBackbone.DataBaseControl.loadFilteredRecipesByName("");
-//  }
 
-  public AutoCompleteRecipeBean() {
-	  recipes = ServeletBackbone.DataBaseControl.loadFilteredRecipesByName("");
+  @PostConstruct
+  public void init() {
+    recipes = ServeletBackbone.DataBaseControl.loadFilteredRecipesByName("");
   }
-  
+
+  // public AutoCompleteRecipeBean() {
+  // recipes = ServeletBackbone.DataBaseControl.loadFilteredRecipesByName("");
+  // }
+
   public void handleSelect(SelectEvent event) {
-	  Object item = event.getObject();
-	  FacesMessage msg = new FacesMessage("Selected", "Item:" + item);
-	  System.out.println("Item " + item + " ListSize = " + recipes.size());
+    Object item = event.getObject();
+    FacesMessage msg = new FacesMessage("Selected", "Item:" + item);
+    recipes = ServeletBackbone.DataBaseControl.loadFilteredRecipesByName(filterString.toUpperCase());
+    System.out.println("Item " + item + " ListSize = " + recipes.size() + " filterString = " + filterString);
+
   }
 
   public List<String> completeText(String query) {
-	recipes.clear();  
     recipes = ServeletBackbone.DataBaseControl.loadFilteredRecipesByName(query.toUpperCase());
-    System.out.println("updatesize = " + recipes.size());
     List<String> results = new ArrayList<String>();
     for (Recipe recipe : recipes) {
       results.add(recipe.getName());
     }
     return results;
-  } 
+  }
 
   public String getFilterString() {
     return filterString;
@@ -64,12 +64,12 @@ public class AutoCompleteRecipeBean {
   public void setFilterString(String filterString) {
     this.filterString = filterString;
   }
-  
+
   public List<Recipe> getRecipes() {
-	return recipes;
+    return recipes;
   }
 
   public void setRecipes(List<Recipe> recipes) {
-	this.recipes = recipes;
+    this.recipes = recipes;
   }
 }
